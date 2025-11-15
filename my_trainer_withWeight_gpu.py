e0a0cb86e6ee06b51a22323ff50a36739bd6c635
green-cabbage <hyeonseo_yun@hotmail.com>
2025-11-15 02:51:15 +0100

    remove ebe mass resolution contribution from class weight balance.  Addresses ram1123/copperheadV2#67.

--- my_trainer_withWeight_gpu.py
+++ my_trainer_withWeight_gpu.py
@@ -816,6 +816,8 @@ def normalizeBdtWgt(df, sig_datasets):
     # print(f"df after: {df[cols]}")
     print(f"df sig after: {df.loc[mask, cols]}")
     print(f"df bkg after: {df.loc[~mask, cols]}")
+    print(f"sig_wgt_sum: {df.loc[mask, "bdt_wgt"].sum()}")
+    print(f"bkg_wgt_sum: {df.loc[~mask, "bdt_wgt"].sum()}")
     # raise ValueError
     return df
 
@@ -858,21 +860,19 @@ def prepare_dataset(df, ds_dict):
     df['bdt_wgt'] = (df['wgt_nominal_orig'])
     df = normalizeBdtWgt(df, sig_datasets)
 
-    print(f"df any neg wgt: {np.any(df['wgt_nominal_orig']<0)}")
-
     # # debugging 
     cols = ['dataset', 'bdt_wgt', 'dimuon_ebe_mass_res',]
-    print(f"df[cols] b4: {df[cols]}")
+    print(f"df[cols] b4 ebe: {df[cols]}")
     # sig
-    for dataset in sig_datasets:
-        ebe_factor = 1
-        df.loc[df['dataset']==dataset,'bdt_wgt'] = df.loc[df['dataset']==dataset,'bdt_wgt'] * ebe_factor*(1 / df[df['dataset']==dataset]['dimuon_ebe_mass_res'])
+    # for dataset in sig_datasets:
+    #     ebe_factor = 1
+    #     df.loc[df['dataset']==dataset,'bdt_wgt'] = df.loc[df['dataset']==dataset,'bdt_wgt'] * ebe_factor*(1 / df[df['dataset']==dataset]['dimuon_ebe_mass_res'])
     # bkg
     # for dataset in bkg_datasets:
     #     ebe_factor = 1
     #     df.loc[df['dataset']==dataset,'bdt_wgt'] = df.loc[df['dataset']==dataset,'bdt_wgt'] * ebe_factor*(1 / df[df['dataset']==dataset]['dimuon_ebe_mass_res']) # FIXME
     # original end -----------------------------------------------
-    print(f"df[cols] after: {df[cols]}")
+    print(f"df[cols] after ebe: {df[cols]}")
 
     # -------------------------------------------------
     # normalize sig dataset again to one
@@ -898,6 +898,8 @@ def prepare_dataset(df, ds_dict):
     print(f"df[cols] after bkg normalization: {df[cols]}")
     print(f'old np.sum(df.loc[mask, "bdt_wgt"]): {bkg_wgt_sum}')
     print(f'new np.sum(df.loc[mask, "bdt_wgt"]): {np.sum(df.loc[mask, "bdt_wgt"])}')
+    print(f're-check sig np.sum(df.loc[mask, "bdt_wgt"]): {np.sum(df.loc[~mask, "bdt_wgt"])}')
+    # raise ValueError
 
     # -------------------------------------------------
     # increase bdt wgts for bdt to actually learn
